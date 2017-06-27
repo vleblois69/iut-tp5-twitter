902051add7d7f560b9afc37321834f1a07c910d0
Vincent LEBLOIS <Vincent LEBLOIS>
2017-06-27 14:18:13 +0200

    Ajout du Feed (Etape 8)

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -9,9 +9,27 @@
 </template>
 
 <script>
+import Tweet from './Tweet'
+
 export default {
   name: 'feed',
-  props: ['tweet']
+  components: {Tweet},
+  props: ['tweets']
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+</style>
 
--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="hello">
-     <ul>
-       <li v-for="tweet in tweets">
-           <tweet :tweet="tweet"/>
-        </li>
-      </ul>
+     <feed :tweets="tweets"/>
   </div>
 </template>
 
 <script>
-import Tweet from './Tweet'
+import Feed from './Feed'
 import Vue from 'vue'
 import Resource from 'vue-resource'
 Vue.use(Resource)
 
 export default {
   name: 'timeline',
-  components: {Tweet},
+  components: {Feed},
   data () {
     return {
-      tweets: []
+      tweets: [{auteur: 'bob'}]
     }
   },
   methods: {
@@ -42,21 +38,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
