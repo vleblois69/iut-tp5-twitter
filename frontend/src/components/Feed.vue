3c952662b570ac4c6cf68f85c8108bbc4fbe22a7
Vincent LEBLOIS <Vincent LEBLOIS>
2017-06-27 15:54:02 +0200

    Amelioration des tweets (Etape 10)

--- frontend/src/components/Feed.vue
+++ frontend/src/components/Feed.vue
@@ -2,7 +2,7 @@
     <div>
       <p v-if="loading">Chargement des tweets en cours...</p>
       <ul v-else>
-        <li v-for="tweet in tweets">
+        <li class="tweet" v-for="tweet in tweets">
           <tweet :tweet="tweet"/>
         </li>
       </ul>
@@ -35,5 +35,16 @@ li {
   margin: 0 10px;
 }
 
+li.tweet:first-child {
+ border-top-width: 1px;
+}
+
+li.tweet {
+ border-style: solid;
+ border-width: 0 1px 1px 1px;
+ border-color: gray;
+ width: 500px;
+ margin: auto;
+}
 </style>
 
--- frontend/src/components/Tweet.vue
+++ frontend/src/components/Tweet.vue
@@ -1,13 +1,52 @@
 <template>
     <div>
-        {{ tweet.auteur.handle }} : {{ tweet.contenu }}
+        <div>
+          <strong>{{ tweet.auteur.prenom }} {{ tweet.auteur.nom }}</strong>
+          <span class="handle">@{{ tweet.auteur.handle }}</span>
+        </div>
+        <div>
+          {{ tweet.contenu }}
+         </div>
+         <div>
+          <ul>
+            <li class="button">
+              <icon name="reply"/>
+            </li>
+            <li class="button">
+              <icon name="retweet"/>
+            </li>
+            <li class="button">
+              <icon name="heart"/>
+            </li>
+            <li class="button">
+              <icon name="envelope"/>
+            </li>
+          </ul>
+         </div>
     </div>
 </template>
 
 <script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
 export default {
   name: 'tweet',
+  components: {Icon},
   props: ['tweet']
 }
 </script>
 
+<style scoped>
+  li.button {
+   display: inline-block;
+  }
+
+  a {
+   color: #42b983;
+  }
+
+  span.handle {
+   color: gray;
+  }
+</style>
