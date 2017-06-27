48e31838fb1af911bbcd81a9e7a42c3ba258c0dc
Vincent LEBLOIS <Vincent LEBLOIS>
2017-06-27 17:56:15 +0200

    Correction Etape 15 pour bien utiliser le composant Utilisateurs

--- frontend/src/components/Timeline.vue
+++ frontend/src/components/Timeline.vue
@@ -1,35 +1,31 @@
 <template>
   <div class="hello">
-    <select @change="onChange($event.target.value)">
-    <option disabled value="">Choisissez un utilisateur ...</option>
-    <option v-for="utilisateur in utilisateurs" :value="utilisateur.handle">
-     {{ utilisateur.prenom }} {{ utilisateur.nom }}
-    </option>
-    </select>
-     <feed :tweets="tweets" :loading="loading" @retweeted="retweet"/>
+    <utilisateurs :utilisateurs="utilisateurs"/>
+    <feed :tweets="tweets" :loading="loading" @retweeted="retweet"/>
   </div>
 </template>
 
 <script>
 import Feed from './Feed'
-import Utilisateur from './Utilisateur'
+import Utilisateurs from './Utilisateurs'
 import Vue from 'vue'
 import Resource from 'vue-resource'
 Vue.use(Resource)
 
 export default {
   name: 'timeline',
-  components: {Feed, Utilisateur},
+  components: {Feed, Utilisateurs},
   data () {
     return {
       tweets: [],
+      utilisateurs: [],
       loading: true
     }
   },
   methods: {
     retweet: function (id) {
       var tweet = this.tweets.find(tweet => tweet.id === id)
-      tweet.retweeters.push({handle: 'snoopdog'})
+      tweet.retweeters.push({handle: 'ladygaga'})
     },
     fetchTweets: function () {
       this.$http.get('http://localhost:8080/list').then(response => {
---
+++ frontend/src/components/Utilisateurs.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <select @change="onChange($event.target.value)">
+        <option disabled value="">Choisissez un utilisateur ...</option>
+        <option v-for="utilisateur in utilisateurs" :value="utilisateur.handle">
+          {{ utilisateur.prenom }} {{ utilisateur.nom }}
+        </option>
+      </select>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'utilisateurs',
+  props: ['utilisateurs']
+}
+</script>
